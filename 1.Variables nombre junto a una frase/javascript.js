86f39fcfbcd3f6b6444ebda71ec1518af029442a
PedroMonrealSa <pmonreals@alumnes.santjosepobrer.es>
2024-03-05 16:04:55 +0100

    Video 1

---
+++ 1.Variables nombre junto a una frase/javascript.js
@@ -0,0 +1,8 @@
+function mostrarNombre(){
+    var miNombre = document.getElementById("miNombre");
+    var nombre = "Pedro";
+    miNombre.textContent = "Mi nombre es " + nombre ;
+
+}
+
+mostrarNombre();
